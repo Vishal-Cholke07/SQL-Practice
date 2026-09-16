0f58921d07d7c9f1ab6ebdf8b3a68267aa1796a5
Vishal-Cholke07 <vishalcholke4313@gmail.com>
2026-09-16 17:54:31 +0530

    WINDOWS FUNCTION Query

---
+++ WINDOW FUNCTION.sql
@@ -0,0 +1,22 @@
+-- WINDOW FUNCTION
+
+SELECT gender, AVG(salary) OVER (PARTITION BY gender)
+FROM employee_demographics dem
+JOIN employee_salary sal
+ON dem.employee_id = sal.employee_id;
+
+-- ROLLING TOTAL 
+
+SELECT dem.first_name, dem.last_name, gender, salary,
+SUM(salary) OVER (PARTITION BY gender ORDER BY dem. employee_id) AS Rolling_Total
+FROM employee_demographics dem
+JOIN employee_salary sal
+ON dem.employee_id = sal.employee_id;
+
+-- ROW NUMBER 
+
+SELECT dem.employee_id,dem.first_name, dem.last_name, gender, salary,
+ROW_NUMBER() OVER (PARTITION BY gender ORDER BY salary DESC)
+FROM employee_demographics dem
+JOIN employee_salary sal
+ON dem.employee_id = sal.employee_id;
